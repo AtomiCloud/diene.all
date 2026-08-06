aa89ef355362b58f198dae367e2e483774f1bd06
adelphi-liong <adelphi@atomi.cloud>
2026-08-06 17:52:42 +0000

    chore(nix): drop the unused nixpkgs-unstable input

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,47 +1,23 @@
-{
-  atomi,
-  pkgs-2605,
-  pkgs-unstable,
-}:
-let
-  all = rec {
-    atomipkgs = (
-      with atomi;
-      {
-        inherit
-          atomiutils
-          cyanprint
-          dlint
-          infralint
-          infrautils
-          releaser
-          skills-sync
-          ;
-      }
-    );
-
-    nix-2605 = (
-      with pkgs-2605;
-      {
-        inherit
-          actionlint
-          git
-          go-task
-          infisical
-          pre-commit
-          shellcheck
-          treefmt
-          ;
-      }
-    );
-
-    nix-unstable = (
-      with pkgs-unstable;
-      {
-      }
-    );
-
-  };
-in
-with all;
-atomipkgs // nix-2605 // nix-unstable
+{ atomi, pkgs-2605 }:
+(with atomi; {
+  inherit
+    atomiutils
+    cyanprint
+    dlint
+    infralint
+    infrautils
+    releaser
+    skills-sync
+    ;
+})
+// (with pkgs-2605; {
+  inherit
+    actionlint
+    git
+    go-task
+    infisical
+    pre-commit
+    shellcheck
+    treefmt
+    ;
+})
